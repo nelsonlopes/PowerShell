fcbbdd67d6b7c67914433de24558e827eaec8de7
Nelson Lopes <nelopes@expressglass.pt>
2020-03-21 12:49:29 +0000

    feat: Add script to convert DOCX to PDF using Office

---
+++ Convert-DOCX-to-PDF.ps1
@@ -0,0 +1,17 @@
+﻿$documents_path = 'C:\Users\nelopes\Desktop\test1'
+
+$word_app = New-Object -ComObject Word.Application
+
+# This filter will find .doc as well as .docx documents
+Get-ChildItem -Path $documents_path -Filter *.doc? | ForEach-Object {
+
+    $document = $word_app.Documents.Open($_.FullName)
+
+    $pdf_filename = "$($_.DirectoryName)\$($_.BaseName).pdf"
+
+    $document.SaveAs([ref] $pdf_filename, [ref] 17)
+
+    $document.Close()
+}
+
+$word_app.Quit()
